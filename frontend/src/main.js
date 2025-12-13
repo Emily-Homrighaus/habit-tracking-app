35a468214b25215cc07fea25734b9fd7054eb6d6
BuildTools <unconfigured@null.spigotmc.org>
2025-12-13 21:04:16 +0000

    Added a mongodb database

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -19,6 +19,13 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
+  <div>
+    <form name="calorieForm">
+      <label for="calorieNumber">How many caloires did you eat today?</label><br>
+      <input type="number" id="calorieNumber" name="calorieNumber" min="0"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 `
 
 setupCounter(document.querySelector('#counter'))
